f4d726cf1cd24cb0a285881148a7311683881ba6
Óscar Juan Bonet <oscjuabon@alu.edu.gva.es>
2025-04-09 16:51:21 +0200

    UI Mobile y pasillos arreglados

---
+++ Proyectos/Proyecto-Final/Proyecto-Final-2DAM/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Velocidad de movimiento
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Captura de input (horizontal y vertical)
+        movement.x = Input.GetAxisRaw("Horizontal"); // A/D o Flechas izquierda/derecha
+        movement.y = Input.GetAxisRaw("Vertical");   // W/S o Flechas arriba/abajo
+
+        // Normaliza el vector para que las diagonales no sean más rápidas
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // Movimiento del personaje
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
