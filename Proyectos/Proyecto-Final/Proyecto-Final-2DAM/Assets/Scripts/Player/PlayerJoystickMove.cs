301dc2806bcd1726d54087741790a11c106a89b8
Óscar Juan Bonet <oscjuabon@alu.edu.gva.es>
2025-05-28 18:33:15 +0200

    Arreglado bug personaje

--- Proyectos/Proyecto-Final/Proyecto-Final-2DAM/Assets/Scripts/Player/PlayerJoystickMove.cs
+++ Proyectos/Proyecto-Final/Proyecto-Final-2DAM/Assets/Scripts/Player/PlayerJoystickMove.cs
@@ -7,9 +7,7 @@ public class PlayerJoystickMove : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
 
-    public Joystick joystick; // solo este joystick
-    public WeaponController weapon;
-
+    public Joystick joystick;
     public int maxHealth = 4;
     public int health = 4;
     private bool isDead = false;
@@ -19,43 +17,45 @@ public class PlayerJoystickMove : MonoBehaviour
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
-    private Vector2 lastDirection = Vector2.right;
-
     void Start()
     {
-        health = maxHealth;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
+        spriteRenderer.flipX = false; // Ahora sí puedes usarlo
+
+        health = maxHealth;
+
         if (joystick == null)
+        {
             joystick = FindObjectOfType<FixedJoystick>();
+        }
 
         if (gameOverManager == null)
+        {
             gameOverManager = FindObjectOfType<GameOverManager>();
+        }
 
         Time.timeScale = 1f;
     }
 
+
     void Update()
     {
-        movement = new Vector2(joystick.Horizontal, joystick.Vertical).normalized;
-
-        if (movement.magnitude > 0.1f)
+        if (joystick != null)
         {
-            lastDirection = movement;
-            animator.SetBool("isWalking", true);
-        }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
+            movement = new Vector2(joystick.Horizontal, joystick.Vertical).normalized;
 
-        if (Mathf.Abs(movement.x) > 0.01f)
-            spriteRenderer.flipX = movement.x < 0;
+            bool isWalking = movement.magnitude > 0.1f;
+            animator.SetBool("isWalking", isWalking);
 
-        if (weapon != null)
-            weapon.Aim(lastDirection);
+            // Giro del sprite horizontalmente
+            if (Mathf.Abs(movement.x) > 0.01f)
+            {
+                spriteRenderer.flipX = movement.x < 0;
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.K)) health = 0;
         if (Input.GetKeyDown(KeyCode.J)) health--;
@@ -74,13 +74,8 @@ public class PlayerJoystickMove : MonoBehaviour
 
     public void Heal(float amount)
     {
-        health = (int)Mathf.Min(health + amount, maxHealth);
+        health = (int)Mathf.Min(health + amount, maxHealth); // No permite superar la vida máxima
     }
 
-    // Llamado por el botón de disparo
-    public void OnShootButtonPressed()
-    {
-        if (weapon != null)
-            weapon.TryShoot();
-    }
-}
+    
+}   
